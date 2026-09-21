823e532d7c34dfc47ab46073c5219560b3a67577
Shchoholiev <sh.codework@gmail.com>
2026-09-21 11:10:56 -0400

    Add sharding function first implementation

--- src/sharder.rs
+++ src/sharder.rs
@@ -1,13 +1,62 @@
-use crate::test_case::Test;
+use std::{cmp::max, collections::{BTreeMap, BinaryHeap}};
+
+use crate::{test_case::Test};
 // use std::collections::BinaryHeap;
 
 #[cfg(test)]
 mod tests;
 
-pub fn shard_tests(tests: Vec<Test>, shard_time_ms: u32) -> Vec<Vec<Test>> {
-    // let shard_time: u32 =
+pub fn shard_tests(tests: Vec<Test>, target_shard_time_ms: u32) -> Vec<Vec<Test>> {
+    let tests_durations: Vec<u32> = tests.iter().map(|test| test.duration_ms).collect();
 
-    // let mut tests_max_heap: BinaryHeap<(u32,)> = BinaryHeap::new();
+    let longest_test_duration: u32 = tests_durations
+      .iter()
+      .max()
+      .copied()
+      .unwrap_or(0);
+    let shard_time_ms = max(longest_test_duration, target_shard_time_ms);
 
-    todo!("not impl");
-}
+    let n_shards: u32 = tests_durations
+      .iter()
+      .sum::<u32>()
+      .div_ceil(shard_time_ms);
+
+    let mut shards: BTreeMap<(u32, usize), Vec<Test>> = 
+      (0..n_shards as usize)
+        .map(
+              |id| 
+              (
+                (shard_time_ms, id)
+                , Vec::new()
+              )
+            )
+        .collect();
+
+    let mut tests_sorted: BinaryHeap<Test> = tests.into();
+    while let Some(test) = tests_sorted.pop() {
+        let test_duration = test.duration_ms;
+        let candidate_key = shards
+          .range((test_duration, 0)..)
+          .next()
+          .map(|(key, _tests)| *key);
+
+        if candidate_key.is_none() {
+          shards.insert((shard_time_ms - test_duration, shards.len()), vec![test]);
+          continue;
+        }
+
+        let Some((remaining_ms, shard_id)) = candidate_key else { continue };
+
+        let mut shard_tests = shards
+          .remove(&candidate_key.unwrap())
+          .unwrap();
+        shard_tests.push(test);
+
+        shards.insert((remaining_ms - test_duration, shard_id), shard_tests);
+    }
+
+    let result: Vec<Vec<Test>> = shards
+      .into_values()
+      .collect();
+    return result
+} 
